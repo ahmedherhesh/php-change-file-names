2bfbfc45a1c044b316b5ef8e26bea77382e7c928
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-07-26 18:20:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$path = '';
+$files = scandir($path);
+$num = 1;
+foreach ($files as $file) {
+    if (in_array($file, ['.', '..']) || is_dir($path . '\\' . $file)) continue;
+    if ($num < 10) $num = '0' . $num;
+    $extention = '.' . array_reverse(explode('.', $file))[0];
+    rename($path . '\\' . $file, $path . '\\' . $num . $extention);
+    $num++;
+}
