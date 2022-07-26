04936a8a1f45724127a147189324f6f1056c18c9
ahmedherhesh <ahmedherhesh3@gmail.com>
2022-07-26 18:36:51 +0200

    final

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
-$path = '';
-$files = scandir($path);
-$num = 1;
-foreach ($files as $file) {
-    if (in_array($file, ['.', '..']) || is_dir($path . '\\' . $file)) continue;
-    if ($num < 10) $num = '0' . $num;
-    $extention = '.' . array_reverse(explode('.', $file))[0];
-    rename($path . '\\' . $file, $path . '\\' . $num . $extention);
-    $num++;
+$path = !empty($_GET['path']) ? $_GET['path'] : '';
+if($path){
+    $files = scandir($path);
+    $num = 1;
+    foreach ($files as $file) {
+        if (in_array($file, ['.', '..']) || is_dir($path . '\\' . $file)) continue;
+        if ($num < 10) $num = '0' . $num;
+        $extention = '.' . array_reverse(explode('.', $file))[0];
+        rename($path . '\\' . $file, $path . '\\' . $num . $extention);
+        $num++;
+    }
 }
